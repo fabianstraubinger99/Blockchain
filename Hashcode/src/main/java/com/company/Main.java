873797ad9e4472d4855864603883b538d21a9f8d
fabianstraubinger99 <fabian.straubinger@gmail.com>
2023-10-12 16:46:13 +0200

    Added Added slf4j

--- Hashcode/src/main/java/com/company/Main.java
+++ Hashcode/src/main/java/com/company/Main.java
@@ -1,32 +1,55 @@
 package com.company;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 public class Main {
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        logger.info("Anwendung startet");
 
+        String input = "{\"name\":\"hendrik\"}";
+
+        String result = getNameFromJson(input);
+
+        System.out.println(result);
+
+
+    }
+
+    public static String getNameFromJson(String json) {
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(json);
+        logger.info("JSON erfolgreich geparst");
+        JsonObject object = element.getAsJsonObject();
+        String name = object.get("name").getAsString();
+        logger.info("Name : {}", name);
+        return name;
+    }
+
+
+    public static void getHashcode() {
         System.out.print("Gib eine Zahl zum hashen ein: ");
         Scanner scanner = new Scanner(System.in);
         long in = scanner.nextLong();
 
-
-        String res = getHashcode(in);
-
-        System.out.println(res);
-
-    }
-
-    public static String getHashcode(long in) {
         String input = Long.toString(in);
         int length = input.length();
 
-
         int res = 0;
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             res += Integer.parseInt(String.valueOf(input.charAt(i)));
         }
 
-        return String.format("%8s", res).replace(' ', '0');
+        System.out.println(String.format("%8s", res).replace(' ', '0'));
+
     }
+
+
 }
