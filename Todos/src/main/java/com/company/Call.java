eb8e707fb417a7529e600279228236d655485475
fabianstraubinger99 <fabian.straubinger@gmail.com>
2024-01-31 22:02:02 +0100

    fixed getMyOpenTodo added finishMyTodo and getMyDoneTodos

--- Todos/src/main/java/com/company/Call.java
+++ Todos/src/main/java/com/company/Call.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import com.esaulpaugh.headlong.abi.Tuple;
 import com.hedera.hashgraph.sdk.*;
 import com.hedera.hashgraph.sdk.proto.SignedTransaction;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeoutException;
 
 public class Call {
@@ -39,22 +43,68 @@ public class Call {
         //Print the account key to the console
         System.out.println(accountInfo);
 
-        ContractId contractId = ContractId.fromString("0.0.7739433");
+        ContractId contractId = ContractId.fromString("0.0.7764873");
 
 
         getOwner(client, contractId);
 
-        addMyNewTodo(client, contractId, "Hello from Java SDK");
+        addMyNewTodo(client, contractId, "Hello " + new Random().nextInt(100));
 
         getMyOpenTodos(client, contractId);
 
+        finishMyTodo(client, contractId, "Hello " + new Random().nextInt(100));
+
+        getMyDoneTodos(client, contractId);
+
+    }
+
+    private static void finishMyTodo(Client client, ContractId contractId, String message) throws TimeoutException, PrecheckStatusException, ReceiptStatusException {
+        // Calls a function of the smart contract
+        ContractExecuteTransaction contractExecuteTransaction = new ContractExecuteTransaction()
+                //Set the contract ID
+                .setContractId(contractId)
+                //Set the gas for the query
+                .setGas(100000)
+                //Set the parameters for the function call
+                .setFunction("finishMyTodo", new ContractFunctionParameters().addString(message))
+                //Set the payment amount for the function call
+                .setMaxTransactionFee(new Hbar(2));
+
+        //Sign with the client operator private key and submit the transaction to a Hedera network
+        TransactionResponse contractExecuteTransactionResponse = contractExecuteTransaction.execute(client);
+
+        //Request the receipt of the transaction
+        contractExecuteTransactionResponse.getReceipt(client);
+    }
+
+    private static void getMyDoneTodos(Client client, ContractId contractId) throws TimeoutException, PrecheckStatusException {
+        // Calls a function of the smart contract
+        ContractCallQuery contractQuery = new ContractCallQuery()
+                //Set the gas for the query
+                .setGas(500000)
+                //Set the contract ID to return the request for
+                .setContractId(contractId)
+                //Set the function of the contract to call
+                .setFunction("getMyDoneTodos")
+                //Set the query payment for the node returning the request
+                //This value must cover the cost of the request otherwise will fail
+                .setQueryPayment(new Hbar(2));
+
+        //Submit to a Hedera network
+        ContractFunctionResult getMessage = contractQuery.execute(client);
+
+        //Get the message
+        List<String> message = getMessage.getStringArray(0);
+
+        //Log the message
+        System.out.println("The contract message: " + message);
     }
 
     private static void getMyOpenTodos(Client client, ContractId contractId) throws TimeoutException, PrecheckStatusException {
         // Calls a function of the smart contract
         ContractCallQuery contractQuery = new ContractCallQuery()
                 //Set the gas for the query
-                .setGas(100000)
+                .setGas(500000)
                 //Set the contract ID to return the request for
                 .setContractId(contractId)
                 //Set the function of the contract to call
@@ -67,11 +117,10 @@ public class Call {
         ContractFunctionResult getMessage = contractQuery.execute(client);
 
         //Get the message
-        String message = getMessage.getAddress(0);
+        List<String> message = getMessage.getStringArray(0);
 
         //Log the message
         System.out.println("The contract message: " + message);
-
     }
 
     private static void addMyNewTodo(Client client, ContractId contractId, String message) throws TimeoutException, PrecheckStatusException, ReceiptStatusException {
